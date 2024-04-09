3b58ebc1d37b6daf911b44d30eebcccb49e11ab0
Nikos Zabakikas <nikoszabakikas@gmail.com>
2024-04-09 19:48:18 +0300

    .

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from Tools import PasswordGenerator
-from Tools import AutoFIlter
+from toolbox.Tools import passgener
+from Tools import AutoFilter
 def main():
     print("\nMokzz: Welcome to my very first...primitive toolbox! You can use this set of tools however you want!\nMokzz: Choose the tool you want to use.\nPassword Generator(1) - AutoFilter(2)")
     tool=int(input("Me: "))
     if tool==1:
-        PasswordGenerator.main()
+        passgener.main()
     elif tool==2:
-        AutoFIlter.main()
+        AutoFilter.main()
         print("Good to go!")
 if __name__=="__main__":
     main()
