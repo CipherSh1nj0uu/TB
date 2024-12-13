05db6e2d84cb03e722ee030f4b1cfc220728e9fe
Nikos Zampakikas <100320490+GitZamp@users.noreply.github.com>
2024-12-13 16:12:17 +0200

    removed some lines

--- tools/passgener.py
+++ tools/passgener.py
@@ -1,44 +1,31 @@
 # Author: GitZamp
 # *Important Message*: Be aware of the comments I leave. Usually they start with hashtag (#example) for single line comments or apostrophe ('''example''') for multiple lines of comments.
-
 # Imports the build-in library 'random'.
 import random
-
 def main():
     def generate_password(pwlength):
         # Modern Latin Alphabet.
         alphabet = "abcdefghijklmnopqrstuvwxyz"
-
         passwords = []
         # This {for} loop mixes the password with letters from the alphabet.
         for x in pwlength:
-
             password = ""
             for y in range(x):
                 next_letter_index = random.randrange(len(alphabet))
                 password = password + alphabet[next_letter_index]
-
             # The 2 lines below call the fuctions "replace_with_number" and "replace_with_uppercase_letter", (line 31 and line 37 respectively).
             password = replace_with_number(password)
             password = replace_with_uppercase_letter(password)
-
             passwords.append(password)
-
         return passwords
-
 # This functions mixes the given password with random numbers.
-
-
     def replace_with_number(pword):
         for i in range(random.randrange(1, 3)):
             replace_index = random.randrange(len(pword) // 2)
             pword = pword[0:replace_index] + \
                 str(random.randrange(10)) + pword[replace_index + 1:]
             return pword
-
 # This function mixes the given password with Uppercase letters.
-
-
     def replace_with_uppercase_letter(pword):
         for i in range(random.randrange(1, 3)):
             replace_index = random.randrange(len(pword) // 2, len(pword))
@@ -47,7 +34,6 @@ def main():
             return pword
     print("How many passwords do you to generate?")
     num_passwords = int(input("Enter a Number: "))
-
     passwords_lengths = []
     # This {for} loop sets the length of the passwords according to your answer.
     for i in range(num_passwords):
@@ -57,12 +43,9 @@ def main():
             length = 3
         if length > 16:
             length = 16
-
         passwords_lengths.append(length)
-
     # Calls 'generate_password' fuction (line 8).
     password = generate_password(passwords_lengths)
-
     # This {for} loop prints the randomized passwords you want.
     for i in range(num_passwords):
         print("Password " + str(i + 1) + " = " + password[i])
